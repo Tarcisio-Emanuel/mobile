5c60e5f8b0ba096ff825279c134fdae39f59d2e1
Tarcisio <tarcisioemanoel10031@gmail.com>
2025-01-31 23:27:53 -0300

    order

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import { View, Text, StyleSheet } from "react-native";
-import { useRoute, RouteProp } from "@react-navigation/native";
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    TextInput,
+} from "react-native";
 
+import { useRoute, RouteProp } from "@react-navigation/native";
+import { Feather } from '@expo/vector-icons'
 
 type RouteDetailParams = {
-    Order:{
+    Order: {
         number: number | string;
         order_id: string;
     }
@@ -13,14 +20,43 @@ type RouteDetailParams = {
 type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>;
 
 export default function Order() {
-const route = useRoute<OrderRouteProps>();    
-    return(
+    const route = useRoute<OrderRouteProps>();
+    return (
         <View style={styles.container}>
-            <Text> Pagina de Order</Text>
-            <Text>{route.params.number}</Text>
-            <Text>{route.params.order_id}</Text>
 
-        </View>
+            <View style={styles.header}>
+                <Text style={styles.title}>Mesa {route.params.number}</Text>
+
+                <TouchableOpacity>
+                    <Feather name='trash-2' size={28} color='#ff3f4b' />
+                </TouchableOpacity>
+            </View>
+
+
+
+
+            <TouchableOpacity style={styles.input}>
+                <Text style={{ color: '#fff' }}>Pizzas</Text>
+            </TouchableOpacity>
+
+
+            <TouchableOpacity style={styles.input}>
+                <Text style={{ color: '#fff' }}>Pizzas Calabresa</Text>
+            </TouchableOpacity>
+
+            <View style={styles.qtdContainer}>
+                <Text style={styles.qtdText}> Quantidade</Text>
+
+                <TextInput
+                style={[styles.input, {width: '60%', textAlign: 'center'}]}
+                    // placeholder='1'
+                    placeholderTextColor='#f0f0f0'
+                    keyboardType='numeric'
+                    value='1'
+                />
+            </View>
+
+        </View> 
     )
 }
 
@@ -31,7 +67,47 @@ const route = useRoute<OrderRouteProps>();
 
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
+        flex: 1,
+        backgroundColor: '#1d1d2e',
+        paddingVertical: '5%',
+        paddingEnd: '4%',
+        paddingStart: '4%'
+    },
+    header: {
+        flexDirection: 'row',
+        marginBottom: 12,
+        alignItems: 'center',
+        marginTop: 24,
 
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginRight: 24,
+    },
+    input: {
+        backgroundColor: '#101026',
+        borderRadius: 4,
+        width: '100%',
+        height: 40,
+        marginBottom: 12,
+        justifyContent: 'center',
+        paddingHorizontal: 8,
+        color: '#fff',
+        fontSize: 20
+    },
+    qtdContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+
+
+    },
+    qtdText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff',
     }
 })
